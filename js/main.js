48e9d83e00843d5b9ec46f0cc401f2c16d77b3f8
Vlad <andreivladpaul@gmail.com>
2021-07-20 14:34:19 +0200

    Esercizio completo

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* 1. chiedi all’utente il cognome
+2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+3. stampa la lista ordinata alfabeticamente
+4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova */
+
+var listSurnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+//1. chiedi all’utente il cognome
+var newSurname = prompt("Qual è il tuo cognome?")
+//inseriscilo in un array con altri cognomi
+listSurnames.push(newSurname);
+
+var listSurnames_copy = listSurnames.slice();
+// var listSurnames_copy = [...listSurnames];  --> altro modo per copiare gli elmenti di un array
+
+// stampa la lista ordinata alfabeticamente
+var sortedSurnames = listSurnames_copy.sort(function (a, b) {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    return 0;
+  });
+
+  console.log(sortedSurnames);
+//scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova 
+console.log((sortedSurnames.indexOf(newSurname)) + 1);
+
